d19308647b404d9f94ad9de3b49e0459f4f59150
Jacob <jacob.siler91@gmail.com>
2019-12-07 18:03:36 +0000

    Initial commit, with project settings, gitignore, and Custom HAP Ride and Dismount scripts.

---
+++ Assets/HAPDismount.cs
@@ -0,0 +1,30 @@
+﻿using MalbersAnimations.HAP;
+using UnityEngine;
+
+public class HAPDismount : MonoBehaviour
+{
+    [Tooltip("A reference to the Ultimate Character Controller camera.")]
+    [SerializeField] private Camera m_Camera;
+    public MRider mRider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mRider=this.GetComponent<MRider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F)) {
+            NewMethod(); }
+    }
+
+    private void NewMethod()
+    {
+        //Enable the UCC camera since there is now a UCC character on the horse again!
+        m_Camera.enabled = true;
+        //Trying to dismount a horse!
+        mRider.DismountAnimal();
+    }
+}
---
+++ Assets/HAPRide.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using MalbersAnimations.HAP;
+using Opsive.UltimateCharacterController.Character;
+using Opsive.UltimateCharacterController.Character.Abilities;
+
+
+
+public class HAPRide : DetectObjectAbilityBase
+{
+    [Tooltip("A reference to the Ultimate Character Controller camera.")]
+    [SerializeField] private Camera m_Camera;
+    [Tooltip("A reference to the Rider Script Component on this character.")]
+    public MRider mRider;
+
+    protected override void AbilityStarted()
+    {
+        base.AbilityStarted();
+        //Disable the UCC camera since there is no UCC character on the horse.
+        if (m_Camera != null)
+        { m_Camera.enabled = false; }
+        else
+        { Debug.LogError("The 'HAPRide' script requires a reference to the Camera with a UCC Camera Controller script attached."); }
+
+    //Trying to mount a horse!
+        mRider.MountAnimal();
+    }
+
+}
